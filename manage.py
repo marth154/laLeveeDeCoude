a6334f0ceb139d4732cf4e12a63171d330fc1bd9
marth154 <martin.danvers@gmail.com>
2022-03-10 10:43:29 +0100

    Init Docker

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'laLeveeDeCoude.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LaLeveeDeCoude.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
