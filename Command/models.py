675411d04fa2cb6ff8beb34f562378eb60883b8b
marth154 <martin.danvers@gmail.com>
2022-03-10 15:28:01 +0100

    Divised model

---
+++ Command/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from User.models import User
+from Ingredient.models import Ingredient
+
+# Create your models here.
+
+
+class Command(models.Model):
+    amount = models.IntegerField(max_length=50)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField('created at')
+
+
+class Command_Ingredients(models.Model):
+    command_id = models.ForeignKey(Command, on_delete=models.CASCADE)
+    ingredient_id = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    quantity = models.IntegerField(max_length=50)
