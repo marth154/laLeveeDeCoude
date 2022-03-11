a82157251e7fc79ef17ae51dd2b10bcf047de4a4
Chloengr <chloe.nger@gmail.com>
2022-03-11 14:50:15 +0100

    :sparkles: edit model for recipe & admin dashbrd

--- Recipe/models.py
+++ Recipe/models.py
@@ -17,15 +17,12 @@ class Recipe(models.Model):
     name = models.CharField(max_length=250)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
-    is_shared = models.BooleanField
-    alcoholic = models.BooleanField
+    is_shared = models.BooleanField(default=False)
+    alcoholic = models.BooleanField(default=True)
     glass_id = models.ForeignKey(Glass, on_delete=models.CASCADE)
-    steps = models.CharField(max_length=500)
+    steps = models.TextField()
+    ingredient_group = models.ManyToManyField(Ingredient)
     created_at = models.DateTimeField('created at')
     updated_at = models.DateTimeField('updated at')
 
 
-class Recipe_Ingredients(models.Model):
-    recipe_id = models.ForeignKey(Recipe, on_delete=models.CASCADE)
-    ingredient_id = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
-    quantity = models.CharField(max_length=250)
