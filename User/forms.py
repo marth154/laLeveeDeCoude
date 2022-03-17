04d6b1881be3b458aaa7188a4feb569f537a46cc
marth154 <martin.danvers@gmail.com>
2022-03-17 09:01:23 +0100

    Supp Validator

--- User/forms.py
+++ User/forms.py
@@ -1,8 +1,7 @@
 from django import forms
-from validators.register import validate_username
 
 class RegisterForms(forms.Form):
-    username = forms.CharField(label='Your username', validators=[validate_username])
+    username = forms.CharField(label='Your username')
     email = forms.CharField(label='Your email')
     password = forms.CharField(label='Your password',widget=forms.PasswordInput())
     # username = forms.CharField(label='Your pseudo')
