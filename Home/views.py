5cb2fb4a5578872f63e0de5b036f3d96af8b621d
Chloengr <chloe.nger@gmail.com>
2022-03-17 16:45:26 +0100

    :eyes: fix path for template in home

--- Home/views.py
+++ Home/views.py
@@ -5,17 +5,17 @@ from Recipe.models import Category, Ingredient, Recipe, User
 
 
 def home(request):
-    if request.user:
-        return render(request, 'home.html')
-    else:
-        print("User isn't authenticated")
-        return render(request, 'home.html')
-        
-def latest_recipe_list(request):
     latest_recipe = Recipe.objects.order_by('created_at')[:4]
     categories = Category.objects.all()
     users = User.objects.all()
     ingredients = Ingredient.objects.all()
     
     context = { "latest_recipe": latest_recipe, "categories": categories, "ingredients": ingredients, "users": users}
-    return render(request, "last-recipe.html", context)
+    if request.user:
+        return render(request, 'home.html', context)
+    else:
+        print("User isn't authenticated")
+        return render(request, 'home.html')
+
+def latest_recipe_list(request):
+    return render(request, "last-recipe.html")
