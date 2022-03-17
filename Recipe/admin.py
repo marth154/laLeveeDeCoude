18ab46762a59eeff9917927c5689d842b20848f0
killiancambert <killian.cambert@ynov.com>
2022-03-17 09:56:25 +0100

    add bootstrap

--- Recipe/admin.py
+++ Recipe/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 
+from .models import Category
+from .models import Glass
 from .models import Recipe
+from .models import Recipe_Ingredients
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Glass)
 admin.site.register(Recipe)
+admin.site.register(Recipe_Ingredients)
