2e324eb42d4ee3fbb464efbb0a0016f489177417
marth154 <martin.danvers@gmail.com>
2022-03-17 14:00:48 +0100

    Debut de mise en forms des login

--- User/forms.py
+++ User/forms.py
@@ -2,13 +2,26 @@ from django import forms
 
 
 class RegisterForms(forms.Form):
-    username = forms.CharField(label='Your username')
-    email = forms.CharField(label='Your email')
+    username = forms.CharField(
+        label='Your username',
+        widget=forms.TextInput(attrs={"placeholder": ("Pseudo")})
+    )
+    email = forms.CharField(
+        label='Your email',
+        widget=forms.TextInput(attrs={"placeholder": ("Email")}),
+    )
     password = forms.CharField(
-        label='Your password', widget=forms.PasswordInput())
+        label='Your password',
+        widget=forms.PasswordInput(attrs={"placeholder": ("Mot de passe")})
+    )
 
 
 class LoginForms(forms.Form):
-    username = forms.CharField(label='Your username')
+    username = forms.CharField(
+        label='Your username',
+        widget=forms.TextInput(attrs={"placeholder": ("Pseudo")}),
+    )
     password = forms.CharField(
-        label='Your password', widget=forms.PasswordInput())
+        label='Your password',
+        widget=forms.PasswordInput(attrs={"placeholder": ("Mot de passe")})
+    )
