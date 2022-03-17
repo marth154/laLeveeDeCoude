b8625db5f21806f125028e73891878669fce3b09
killiancambert <killian.cambert@ynov.com>
2022-03-17 16:04:14 +0100

    rebase()

--- Recipe/views.py
+++ Recipe/views.py
@@ -1,18 +1,20 @@
+from operator import concat
 from django.shortcuts import render
 import requests
 
 def random(request):
     response = requests.get('http://www.thecocktaildb.com/api/json/v1/1/random.php').json()
-    print(response);
-    return render(request, 'random.html', {'response': response})
+    print('🚀 ~ file: views.py ~ line 7 ~ response', response);
+    parsedResponse = formatRecipe(response['drinks'][0])
+    return render(request, 'random.html', {'response': parsedResponse})
 
 def formatRecipe(recipe):
-    print(recipe)
     recipeIngredients = []
     for i in range(1, 15):
         ing = recipe['strIngredient' + str(i)]
-        if (ing is not None):
-            recipeIngredients.append(ing)
+        measure = recipe['strMeasure' + str(i)]
+        if ((ing is not None and measure is not None) and (ing != '' and measure != '')):
+            recipeIngredients.append(ing + ' : ' + measure)
             
     return { 'idDrink': recipe['idDrink'], 'strDrink': recipe['strDrink'], 'strIngredients': recipeIngredients, 'strCategory': recipe['strCategory'], 'strGlass': recipe['strGlass'], 'strInstructions': recipe['strInstructions'], 'strAlcoholic': recipe['strAlcoholic'], 'strDrinkThumb': recipe['strDrinkThumb'] }
 
