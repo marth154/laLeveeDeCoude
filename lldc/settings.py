c543e95671d4dd2d1884d2a4b8a47bb6eac43730
mariegautron <marigaut2@gmail.com>
2022-04-01 09:51:41 +0200

    Variables env and :art: repsonsive

--- lldc/settings.py
+++ lldc/settings.py
@@ -13,16 +13,23 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 import django_heroku
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
+# read th .env file
+environ.Env.read_env(env_file=str(BASE_DIR) + '/.env')  
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g(j3sv0m5_$yx1u*9y(w)ijrpqf^)q0%%bgw!)(7^41$&igh41'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -48,7 +55,8 @@ INSTALLED_APPS = [
     "Migration",
     'Command',
     'compressor',
-    'fontawesomefree'
+    'fontawesomefree',
+    'meta',
 ]
 
 MIDDLEWARE = [
@@ -97,6 +105,20 @@ DATABASES = {
         'PORT': 5432
     }
 }
+
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
